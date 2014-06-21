22b123d464a2e023f7db9644eab5b48ab67cd8f0
drif <drif@drifs-MacBook-Pro.local>
2014-06-21 18:37:45 +0300

    updated tests

--- tests.py
+++ tests.py
@@ -1,3 +1,6 @@
+# -*- coding: utf-8 -*-
+
+
 import bouquet
 import unittest
 
@@ -7,10 +10,32 @@ class BouquetTestCase(unittest.TestCase):
     def setUp(self):
         self.app = bouquet.app.test_client()
 
-    def test_index(self):
-        resp = self.app.get('/')
-        assert resp.status_code == 200
-        assert 'Hello World!' in resp.data
+    def test_post_compliment(self):
+        resp = self.app.post('/compliment', data={'sex': 'male', 'lang': 'ru', 'text': 'Ты самый лучший!'})
+        assert 'Ты самый лучший!' in resp.data
+
+    def test_get_compliments(self):
+        self.app.post('/compliment', data={'sex': 'male', 'lang': 'ru', 'text': 'Ты самый лучший!'})
+        self.app.post('/compliment', data={'sex': 'female', 'lang': 'ru', 'text': 'Ты самая лучшая!'})
+
+        resp = self.app.get('/compliment')
+        assert 'Ты самый лучший!' in resp.data
+        assert 'Ты самая лучшая!' in resp.data
+
+    def test_delete_compliment(self):
+        self.app.post('/compliment', data={'sex': 'male', 'lang': 'ru', 'text': 'Ты самый лучший!'})
+        self.app.delete('/compliment/1')
+
+        resp = self.app.get('/compliment')
+        assert 'Ты самый лучший!' not in resp.data
+
+    def test_put_compliment(self):
+        self.app.post('/compliment', data={'sex': 'male', 'lang': 'ru', 'text': 'Ты самый лучший!'})
+        self.app.put('/compliment/1', data={'sex': 'female', 'lang': 'ru', 'text': 'Ты самая лучшая!'})
+
+        resp = self.app.get('/compliment')
+        assert 'Ты самый лучший!' not in resp.data
+        assert 'Ты самая лучшая!' in resp.data
 
 
 if __name__ == '__main__':
