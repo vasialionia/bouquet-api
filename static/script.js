ffd23f42f2872a5f8804a06ad0bdf34e03c28e43
drif <drif@drifs-MacBook-Pro.local>
2014-06-22 13:22:28 +0300

    editing compliments

--- static/script.js
+++ static/script.js
@@ -25,6 +25,8 @@ document.addEventListener('DOMContentLoaded', function(){
     };
 
     updateDeleteButtonsListeners();
+    updateEditButtonsListeners();
+    updateSaveButtonsListeners();
     updateCreateButtonVisability();
 });
 
@@ -35,6 +37,30 @@ function updateDeleteButtonsListeners(){
             var compliment_id = input_delete[i].id;
             input_delete[i].onclick = function(){
                 deleteCompliment(compliment_id);
+            };
+        })();
+    }
+}
+
+function updateEditButtonsListeners(){
+    var input_edit = document.getElementsByClassName('compliment_edit');
+    for(var i = 0; i < input_edit.length; i ++){
+        (function(){
+            var compliment_id = input_edit[i].id;
+            input_edit[i].onclick = function(){
+                startEditingCompliment(compliment_id);
+            };
+        })();
+    }
+}
+
+function updateSaveButtonsListeners(){
+    var input_save = document.getElementsByClassName('compliment_save');
+    for(var i = 0; i < input_save.length; i ++){
+        (function(){
+            var compliment_id = input_save[i].id;
+            input_save[i].onclick = function(){
+                saveEditedCompliment(compliment_id);
             };
         })();
     }
@@ -69,6 +95,7 @@ function createCompliment(){
         row.insertCell(-1).innerHTML = '<input id="' + response['id'] + '" class="compliment_delete" type="submit" value="Delete">';
 
         updateDeleteButtonsListeners();
+        updateEditButtonsListeners();
     });
 
     input_text.value = '';
@@ -86,6 +113,52 @@ function deleteCompliment(compliment_id){
             }
         }
     });
+}
+
+function startEditingCompliment(compliment_id){
+    var table = document.getElementById('compliments_table');
+    var rows = table.rows;
+    for(var i = 0; i < rows.length; i ++){
+        if(rows[i].id == compliment_id){
+            var oldRow = rows[i];
+            table.deleteRow(i);
+            var newRow = table.insertRow(i);
+            newRow.id = compliment_id;
+            newRow.insertCell(-1).innerHTML = '<input type="text" placeholder="lang" value="' + oldRow.cells[0].innerHTML + '" style="width:25px; font-size:12px">';
+            newRow.insertCell(-1).innerHTML = '<input type="text" placeholder="sex" value="' + oldRow.cells[1].innerHTML + '" style="width:50px; font-size:12px">';
+            newRow.insertCell(-1).innerHTML = '<input type="text" placeholder="text" value="' + oldRow.cells[2].innerHTML + '" style="width:100%; font-size:12px">';
+            var lastCell = newRow.insertCell(-1);
+            lastCell.colSpan = 2;
+            lastCell.innerHTML = '<input id="' + compliment_id + '" class="compliment_save" type="submit" value="Save">';
+            lastCell.style = 'text-align:center';
+            updateSaveButtonsListeners();
+            break;
+        }
+    }
+}
+
+function saveEditedCompliment(compliment_id){
+    var table = document.getElementById('compliments_table');
+    var rows = table.rows;
+    for(var i = 0; i < rows.length; i ++){
+        if(rows[i].id == compliment_id){
+            var oldRow = rows[i];
+            performRequest('PUT', '/compliment/' + compliment_id, 'lang=' + oldRow.cells[0].firstChild.value + '&sex=' + oldRow.cells[1].firstChild.value + '&text=' + oldRow.cells[2].firstChild.value);
+
+            table.deleteRow(i)
+            var newRow = table.insertRow(i);
+            newRow.id = oldRow.id;
+            newRow.insertCell(-1).innerHTML = oldRow.cells[0].firstChild.value;
+            newRow.insertCell(-1).innerHTML = oldRow.cells[1].firstChild.value;
+            newRow.insertCell(-1).innerHTML = oldRow.cells[2].firstChild.value;
+            newRow.insertCell(-1).innerHTML = '<input id="' + oldRow.id + '" class="compliment_edit" type="submit" value="Edit">';
+            newRow.insertCell(-1).innerHTML = '<input id="' + oldRow.id + '" class="compliment_delete" type="submit" value="Delete">';
+
+            updateDeleteButtonsListeners();
+            updateEditButtonsListeners();
+            break;
+        }
+    }
 }
 
 function performRequest(method, path, params, callback){
