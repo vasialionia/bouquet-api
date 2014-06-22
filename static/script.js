8d9863f76986acc50a7ee552e59a05c900fb9f58
drif <drif@drifs-MacBook-Pro.local>
2014-06-22 12:43:00 +0300

    deleting compliments

--- static/script.js
+++ static/script.js
@@ -15,17 +15,30 @@ document.addEventListener('DOMContentLoaded', function(){
     };
     input_text.onkeydown = function(event){
         if(event.key == 'Enter' && isCreatingAllowed()){
-            onCreateButtonClick();
+            createCompliment();
         }
     };
 
     var input_submit = document.getElementById('input_submit');
     input_submit.onclick = function(){
-        onCreateButtonClick();
+        createCompliment();
     };
 
+    updateDeleteButtonsListeners();
     updateCreateButtonVisability();
 });
+
+function updateDeleteButtonsListeners(){
+    var input_delete = document.getElementsByClassName('compliment_delete');
+    for(var i = 0; i < input_delete.length; i ++){
+        (function(){
+            var compliment_id = input_delete[i].id;
+            input_delete[i].onclick = function(){
+                deleteCompliment(compliment_id);
+            };
+        })();
+    }
+}
 
 function isCreatingAllowed(){
     var input_lang = document.getElementById('input_lang');
@@ -40,38 +53,53 @@ function updateCreateButtonVisability(){
     input_submit.style = isCreatingAllowed() ? '' : 'display:none';
 }
 
-function onCreateButtonClick(){
-    createCompliment();
-}
-
 function createCompliment(){
     var input_lang = document.getElementById('input_lang');
     var input_sex = document.getElementById('input_sex');
     var input_text = document.getElementById('input_text');
 
     performRequest('POST', '/compliment', 'lang=' + input_lang.value + '&sex=' + input_sex.value + '&text=' + input_text.value, function(response){
-            var table = document.getElementById('compliments_table');
-            var row = table.insertRow(-1);
-            row.insertCell(-1).innerHTML = response['lang'];
-            row.insertCell(-1).innerHTML = response['sex'];
-            row.insertCell(-1).innerHTML = response['text'];
-            row.insertCell(-1);
-            row.insertCell(-1);
+        var table = document.getElementById('compliments_table');
+        var row = table.insertRow(-1);
+        row.id = response['id'];
+        row.insertCell(-1).innerHTML = response['lang'];
+        row.insertCell(-1).innerHTML = response['sex'];
+        row.insertCell(-1).innerHTML = response['text'];
+        row.insertCell(-1).innerHTML = '<input id="' + response['id'] + '" class="compliment_edit" type="submit" value="Edit">';
+        row.insertCell(-1).innerHTML = '<input id="' + response['id'] + '" class="compliment_delete" type="submit" value="Delete">';
+
+        updateDeleteButtonsListeners();
     });
 
     input_text.value = '';
     input_text.focus();
 }
 
+function deleteCompliment(compliment_id){
+    performRequest('DELETE', '/compliment/' + compliment_id, undefined, function(){
+        var table = document.getElementById('compliments_table');
+        var rows = table.rows;
+        for(var i = 0; i < rows.length; i ++){
+            if(rows[i].id == compliment_id){
+                table.deleteRow(i);
+                break;
+            }
+        }
+    });
+}
+
 function performRequest(method, path, params, callback){
     var request = new XMLHttpRequest();
     request.open(method, path, true);
-    request.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-    request.setRequestHeader("Content-length", params.length);
+    if(params){
+        request.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+        request.setRequestHeader("Content-length", params.length);
+    }
     request.setRequestHeader("Connection", "close");
     request.onreadystatechange = function(){
         if(callback && request.readyState == 4 && request.status == 200){
-            callback(JSON.parse(request.responseText));
+            var response = request.responseText.length > 0 ? JSON.parse(request.responseText) : undefined;
+            callback(response);
         }
     }
     request.send(params);
