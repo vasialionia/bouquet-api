d06a9c582d759a6ba9f67971e9d29e86d2d849b4
drif <drif@drifs-MacBook-Pro.local>
2015-03-18 10:52:30 +0300

    added authentification

--- app.py
+++ app.py
@@ -1,4 +1,8 @@
 from flask import Flask
+from flask import session
+from flask import redirect
+from flask import request
+from flask import url_for
 from flask import render_template
 from flask.ext.sqlalchemy import SQLAlchemy
 import config
@@ -16,10 +20,38 @@ import views
 views.ComplimentView.register(app)
 
 
+def is_logged_in():
+    return session.get('login') == app.config['ADMIN_LOGIN'] and session.get('pass') == app.config['ADMIN_PASS']
+
+
 @app.route('/')
 def index():
-    return render_template('index.html', compliments=models.Compliment.query.order_by(models.Compliment.lang, models.Compliment.sex, models.Compliment.text).all())
+    if is_logged_in():
+        return redirect(url_for('compliments'))
+    else:
+        return redirect(url_for('login'))
 
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'GET':
+        return render_template('login.html')
+    else:
+        session['login'] = request.form.get('login')
+        session['pass'] = request.form.get('pass')
+        return redirect(url_for('index'))
+
+@app.route('/logout', methods=['POST'])
+def logout():
+    session['login'] = None
+    session['pass'] = None
+    return redirect(url_for('index'))
+
+@app.route('/compliments')
+def compliments():
+    if not is_logged_in():
+        return redirect(url_for('index'))
+    else:
+        return render_template('index.html', compliments=models.Compliment.query.order_by(models.Compliment.lang, models.Compliment.sex, models.Compliment.text).all())
 
 if __name__ == '__main__':
     app.run()
--- config.py
+++ config.py
@@ -5,7 +5,10 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 
 class Config(object):
+    SECRET_KEY = 'secret_key'
     SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
+    ADMIN_LOGIN = 'login'
+    ADMIN_PASS = 'pass'
 
 
 class DebugConfig(Config):
